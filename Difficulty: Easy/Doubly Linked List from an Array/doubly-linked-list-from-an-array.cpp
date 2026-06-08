c681906d2e61a0697bdd2e38cb316984d154029d
MUKUL PRASAD <mukulprasad958@gmail.com>
2026-06-09 01:20:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Doubly Linked List from an Array/doubly-linked-list-from-an-array.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+        prev = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* createDLL(vector<int>& arr) {
+        // code here
+        Node* head=new Node(arr[0]);
+        Node* prev=head;
+        for(int i=1;i<arr.size();i++)
+        {
+            Node* temp=new Node(arr[i]);
+           prev->next = temp;
+            temp->prev = prev;
+
+            prev = temp;
+            
+        }
+        return head;
+    }
+};
